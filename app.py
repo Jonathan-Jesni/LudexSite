fc52181df9b9f3a61072cbce26a9cc210b851bc8
JONATHAN JESNI MANISSERY <83708080+Jonathan-Jesni@users.noreply.github.com>
2025-11-28 03:15:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ app.secret_key = os.environ.get(
 )
 
 STEAM_API_KEY = os.environ.get("STEAM_API_KEY", "B0628D7BA865C799E6D9679396DC563B")
-realm = "ludex.up.railway.app"
-return_to = "ludex.up.railway.app/authorize"
+realm = "https://ludex.up.railway.app/"
+return_to = "https://ludex.up.railway.app//authorize"
 
 
 # ======================================================
